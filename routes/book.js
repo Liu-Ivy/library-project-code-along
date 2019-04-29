dbbce50726b775e3f1bcd60a38ec6ff6096475cb
ross-u <cirkovic.uros.srb@gmail.com>
2019-04-29 12:01:09 +0200

    Update 1

--- routes/book.js
+++ routes/book.js
@@ -21,6 +21,33 @@ router.post('/add', function(req, res, next) {
     .catch( (err) => console.log(err));
 });
 
+//  GET '/books/edit'
+router.get('/edit', (req, res, next) => {
+  const { _id } = req.query;
+
+  Book.findOne({ _id })
+    .then( (book) => res.render('books-edit', { book } ))
+    .catch( (err) => console.log(err));
+});
+
+//  POST '/books/edit'
+router.post('/edit', (req, res, next) => {
+  const { _id } = req.query;
+  const {title, author, description, rating} = req.body;
+
+  Book.findOneAndUpdate( { _id }, { $set:  {title, author, description, rating} })
+    .then( (book) => res.redirect('/books'))
+    .catch( (err) => console.log(err));
+});
+
+
+
+//  GET '/books/details/:bookId'
+router.get('/details/:bookId', (req, res, next) => {
+  console.log('req.params', req.params);
+});
+
+
 // GET '/books'
 router.get('/', (req, res, next) => {
   Book.find({})
