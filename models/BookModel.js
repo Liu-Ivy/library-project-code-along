688b4658f440fd4a2a1aac2204dc73b2546480fc
ross-u <cirkovic.uros.srb@gmail.com>
2019-04-29 15:14:33 +0200

    Code along done again

--- models/BookModel.js
+++ models/BookModel.js
@@ -1,8 +1,4 @@
 const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
-const Author = require('./AuthorModel');
-const reviewSchema = require('./schemas/reviewSchema');
-
 
 const bookSchema = require('./schemas/bookSchema');
 const Book = mongoose.model("Book", bookSchema);
